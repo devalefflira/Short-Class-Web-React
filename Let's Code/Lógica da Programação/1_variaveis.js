eb59f06e4aa9338b69aea316262148bbfb800301
Aleff Lira <dev.alefflira@gmail.com>
2022-03-15 15:15:21 -0300

    Primeiro Commit

---
+++ Let's Code/Lógica da Programação/1_variaveis.js
@@ -0,0 +1,37 @@
+// 1. Variáveis
+
+// 1.1 Tipo: Primitivos
+
+// 1.1.1 boolean
+var a = true;
+var b = false;
+
+// 1.1.2 Number
+var c = 10;
+var d = 5.5;
+
+// 1.1.3 String
+var e = "teste";
+
+// 1.1.4 Undefined
+var f;
+var g = undefined;
+
+// 1.2 Tipo: Complexos
+
+// 1.2.1 function
+var a = function() {};
+var b = () => {};
+
+// 1.2.2 object
+var c = {};
+var d = []; //array
+var e = null;
+
+// 1.3 Const
+
+const pi = 3.1415;
+
+// 1.4 Let
+
+let nome = "Junior";
---
+++ Let's Code/Lógica da Programação/2_condicionais.js
@@ -0,0 +1,76 @@
+// Variáveis
+let num1 = 10;
+let num2 = 5;
+let x = 2;
+let y = 3;
+let z = 4;
+let a = 2;
+let b = 3;
+let c = 4;
+
+// 2. Condicionais
+
+// 2.1 Operadores de Comparação
+
+// 2.1.1 ">" (maior que)
+if(num1 > num2) {
+    console.log("10 é maior que 5");
+}
+// 2.1.2 "<" (menor que)
+if(num2 < num1) {
+    console.log("5 é menor que 10");
+}
+// 2.1.3 ">=" (maior ou igual a)
+if(num1 >= num2) {
+    console.log("10 é maior ou igual a 5");
+}
+// 2.1.4 "<=" (menor ou igual a)
+if(num2 <= num1) {
+    console.log("5 é menor ou igual a 10");
+}
+// 2.1.5 "==" (igual a)
+if(num1 == num2) {
+    console.log("10 é igual a 5");
+}
+else {
+    console.log("10 não é igual a 5");
+}
+// 2.1.6 "===" (igual em valor e tipo)
+if(num1 === num2) {
+    console.log("10 é igual em valor e tipo a 5");
+}
+else {
+    console.log("10 não é igual em valor mas é em tipo (number) a 5")
+}
+// 2.1.7 "!=" (diferente de)
+if(num1 != num2) {
+    console.log("10 é diferente de 5");
+}
+// 2.1.8 "!==" (diferente em valor ou tipo)
+if(num1 !== num2) {
+    console.log("10 é diferente em valor ou em tipo de 5");
+}
+
+// 2.2 Operadores Lógicos
+
+// 2.2.1 E &&
+if(num1 > num2 && x == a) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+// 2.2.2 Ou ||
+if(num1 < num2 || x != y) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+// 2.2.3 Não !
+if(!(x === a)) {
+    console.log(false);
+}
+else {
+    console.log(true);
+}
